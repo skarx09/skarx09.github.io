caa1c94f88b690f9470eb240ed4e488023307715
skarx09 <42556206+skarx09@users.noreply.github.com>
2018-11-13 13:38:39 -0200

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,42 @@
+var btn;
+var cap;
+var juros;
+var saldo;
+var qtmeses;
+var cont;
+var acumulo;
+var erase;
+function init(){
+	btn = document.getElementById("btn_calc");
+	btn.addEventListener("click", txtdelete);
+	btn.addEventListener("click", test );
+}
+function txtdelete(){	
+	erase = document.getElementById("tempototal");
+	erase.innerHTML='';		
+}
+function test(){
+	cap = Number(document.getElementById("CapIni").value);
+	juros = Number(document.getElementById("Juros").value);
+	qtmeses = Number(document.getElementById("Time").value);
+	if(Number.isNaN(cap) || Number.isNaN(juros) || Number.isNaN(qtmeses)){
+		var rr = document.getElementById("result");
+		rr.innerHTML = "Valor(es) Inválido(os), favor corrigir!";	
+	}
+	else{
+		calcula();
+	}
+}
+function calcula(){
+	for(cont= 1;cont <= qtmeses;cont++){
+		saldo = cap*Math.pow(1+(juros/100),cont);
+		acumulo = document.getElementById("tempototal");
+		var pct = saldo.toFixed(2);
+		acumulo.innerHTML += "Capital durante o "+cont+"°" +"Mês é: "+pct+"." + "<br>";
+		document.body.appendChild(acumulo);
+	}
+	saldo = cap*Math.pow(1+(juros/100),qtmeses);
+	var r = document.getElementById("result");
+	r.innerHTML = "Seu Saldo:" + pct;
+}
+init();
